dc44f3a35011bf34d654b87eef5f28f810b33ed9
Nikola Marunic <nikimarunic@gmail.com>
2020-11-18 21:46:37 -0500

    Changed order by in q2 to be just by city qnd numcategories

--- q2.sql
+++ q2.sql
@@ -7,12 +7,14 @@ DROP TABLE IF EXISTS q2 cascade;
 CREATE TABLE q2 (
     hotel TEXT NOT NULL,
     city TEXT NOT NULL,
+    avgCityRating FLOAT NOT NULL,
     numCategories INT NOT NULL,
     rating FLOAT NOT NULL
 );
 
 DROP VIEW IF EXISTS numcategories CASCADE;
 DROP VIEW IF EXISTS avgrating CASCADE;
+DROP VIEW IF EXISTS cityratings CASCADE;
 
 -- 1. Create view with hotels to num categories
 CREATE VIEW numcategories AS 
@@ -26,14 +28,20 @@ SELECT hotelID, avg(rating) AS rating
 FROM reviews
 GROUP BY hotelID;
 
--- 3. Join with 
+-- 3. Create view to capture avg hotel rating per city
+CREATE VIEW cityratings as
+SELECT city AS cityID, avg(rating) as avgCityRating
+FROM reviews NATURAL JOIN hotels
+GROUP BY city;
+
+-- 4. Join with 
 CREATE VIEW result AS
-SELECT hotels.name AS hotel, cities.name as city, numcategories, rating
+SELECT hotels.name AS hotel, cities.name as city, avgCityRating, numcategories, rating
 FROM numcategories NATURAL JOIN avgrating
 JOIN hotels ON hotels.hotelID = numcategories.hotelID
 JOIN cities ON cities.cityID = hotels.city
-ORDER BY numCategories DESC, rating DESC
+JOIN cityratings ON cityratings.cityID = hotels.city
+ORDER BY city DESC, numcategories DESC
 ;
-
 
 insert into q2 SELECT * FROM result;
