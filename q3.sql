cbc00b2a89f2f97f3544fc744252e9e15e0450e8
Nikola Marunic <nikimarunic@gmail.com>
2020-11-18 21:15:36 -0500

    finished q3

--- q3.sql
+++ q3.sql
@@ -4,21 +4,33 @@ SET SEARCH_PATH TO hotelreviews;
 DROP TABLE IF EXISTS q3 cascade;
 
 CREATE TABLE q3 (
-    hotel TEXT NOT NULL,
-    city TEXT NOT NULL,
-    numCategories INT NOT NULL,
-    rating FLOAT NOT NULL
+    user TEXT NOT NULL,
+    numReviews INT NOT NULL,
+    avgScore FLOAT NOT NULL
 );
 
-DROP VIEW IF EXISTS numcategories CASCADE;
+DROP VIEW IF EXISTS numReviews CASCADE;
 DROP VIEW IF EXISTS avgrating CASCADE;
 
--- 1. Create view with hotels to num categories
+-- 1. Create view with user to hum reviews
+CREATE VIEW numReviews AS
+SELECT userID, count(*) AS numReviews
+FROM reviews
+GROUP BY userID;
 
--- 2. Create view hotel to avg rating
+-- 2. Create view user to average score
+CREATE VIEW avgrating AS
+SELECT userID, avg(rating) AS avgScore
+FROM reviews
+GROUP BY userID;
 
-
--- 3. Join with 
+-- 3. Join with each other and get name from user
+CREATE VIEW result AS
+SELECT name, numReviews, avgScore
+FROM avgrating NATURAL JOIN numReviews
+JOIN users ON avgrating.userID = users.userID
+ORDER BY numReviews DESC
+;
 
 
 insert into q3 SELECT * FROM result;
